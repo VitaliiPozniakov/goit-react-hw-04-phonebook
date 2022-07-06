0ae8c29afa49394d69a0aab982aa1cafd902b79a
VitaliiPozniakov <vertalen89@gmail.com>
2022-07-06 20:23:04 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,106 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import ContactForm from './ContactForm';
+import { nanoid } from 'nanoid';
+import ContactList from './ContactList';
+import Filter from './Filter';
+import Section from './Section';
+import { Container } from './App.styled';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import Notification from './Notification';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  componentDidMount () {
+const contactFromLocalStorage = localStorage.getItem('contacts')
+const ParsedContactFromLocalStorage =  JSON.parse(contactFromLocalStorage)
+ParsedContactFromLocalStorage && this.setState({contacts: ParsedContactFromLocalStorage})
+  }
+  
+  componentDidUpdate (prevProps, prevState) {
+ if (this.state.contacts !== prevState.contacts) {
+  localStorage.setItem('contacts', JSON.stringify(this.state.contacts) )
+ }
+  }
+ 
+
+  addContact = (name, number) => {
+    // console.log(name, number)
+
+    const contact = {
+      id: nanoid(10),
+      name,
+      number,
+    };
+
+    if (
+      name.trim() === null ||
+      name.trim() === `` ||
+      number.trim() === null ||
+      number.trim() === ``
+    ) {
+      Notify.warning(`all fields must be completed`);
+      return;
+    }
+
+    this.state.contacts.some(contact => contact.name === name)
+      ? Notify.warning(`${name} is already in contact`)
+      : this.setState(({ contacts }) => ({
+          contacts: [contact, ...contacts],
+        }));
+  };
+
+  changeFilter = evt => {
+    this.setState({ filter: evt.currentTarget.value });
+  };
+
+  getVisibleContacts = () => {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  render() {
+    const visibleContacts = this.getVisibleContacts();
+    const { contacts, filter } = this.state;
+    return (
+      <Container>
+        <Section title="Phonebook">
+          <ContactForm onSubmitProp={this.addContact} />
+        </Section>
+
+        <Section title="Contacts">
+          {contacts.length > 1 && (
+            <Filter value={filter} onChange={this.changeFilter} />
+          )}
+          {contacts.length > 0 ? (
+            <ContactList
+              contacts={visibleContacts}
+              onDeleteContact={this.deleteContact}
+            />
+          ) : (
+            <Notification message="Your contactlist is empty" />
+          )}
+        </Section>
+      </Container>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import css from './ContactForm.module.css';
+import PropTypes from 'prop-types';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as yup from 'yup';
+
+const schema = yup.object().shape({
+  name: yup.string().min(2).max(32).trim().matches().required('Please enter name'),
+  number: yup.number().required('Please enter number'),
+});
+
+const initialValues = {
+  name: '',
+  number: '',
+};
+
+const ContactForm = ({ onSubmitProp }) => {
+  const handleSubmit = (values, actions) => {
+    const { name, number } = values;
+    onSubmitProp(name, number);
+    actions.resetForm();
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      validationSchema={schema}
+    >
+      <Form className={css.forma}>
+        <label htmlFor="name" className={css.label}>
+          <span className={css.labelText}>Name</span>
+          <Field type="text" name="name" className={css.input} />
+          <ErrorMessage name="name" component="span" className={css.error} />
+        </label>
+        <label htmlFor="number" className={css.label}>
+          <span className={css.labelText}>Number</span>
+          <Field type="tel" name="number" className={css.input} />
+          <ErrorMessage name="number" component="span" className={css.error} />
+        </label>
+        <div className={css.btnWraper}>
+          <button type="submit" className={css.btn}>
+            Add contact
+          </button>
+        </div>
+      </Form>
+    </Formik>
+  );
+};
+
+export default ContactForm;
+
+ContactForm.prototype = {
+  onSubmitProp: PropTypes.func.isRequired,
+};
+
+// const Forma = styled(Form)`
+// display: flex;
+// flex-direction: column;
+// border: 1px solid rgba(238, 238, 238, 1);
+// width: 100wh;
+
+// @media screen and (min-width: 768px) {
+//     width: 700px;
+//   }
+// `;
